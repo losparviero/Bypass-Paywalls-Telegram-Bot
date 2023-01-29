f7240d2d30afc3e0a31e1f968c4d147f53b7b791
Zubin <me@zubin.org.in>
2023-01-29 21:03:28 +0530

    Improvements

--- bot.js
+++ bot.js
@@ -1,46 +1,56 @@
-const { Bot, Context, session } = require("grammy");
-const { run, sequentialize } = require("@grammyjs/runner");
+const { Bot } = require("grammy");
 require('dotenv').config();
 const urlRegex = /(https?:\/\/[^\s]+)/g;
 
-// Create a bot.
+// Bot
+
 const bot = new Bot(process.env.BOT_TOKEN);
 
-// Build a unique identifier for the `Context` object.
-function getSessionKey(ctx) {
-  return ctx.chat?.id.toString();
-}
+// Commands
 
-// Sequentialize before accessing session data.
-bot.use(sequentialize(getSessionKey));
-bot.use(session({ getSessionKey }));
-
-// Respond to the command /start.
 bot.command("start", (ctx) => 
-  ctx.reply("<b>Welcome!</b> ✨ Send a webpage or article behind a paywall and we will remove it for you.", { parse_mode: "HTML" },));
+  ctx.reply("*Welcome!* ✨ Send a webpage or article behind a paywall.", { parse_mode: "Markdown" } ));
 bot.command("help", (ctx) =>
-  ctx.reply("This bot uses the 12ft.io proxy service to unblock and bypass paywalls. It's not guaranteed to work with all websites and relies on the status of the proxy service. \ Both are free services hosted and operational on voluntary basis subject to shutdown at any point in time. This bot is hosted on my personal VPS and will stay online for the foreseeable period.", { parse_mode: "MarkdownV2" },));
-bot.command("carla", (ctx) => ctx.reply("You mean the pretty girl reading this?"));
+  ctx.reply("*@anzubo Project.*\n\nThis bot uses the 12ft.io proxy service to unblock and bypass paywalls. It's not guaranteed to work with all websites and relies on the status of the proxy service.\n\n*Sites such as NYT, WSJ or The Economist are not known to work.*\n_Access restricted member-only sites will also not work._", { parse_mode: "Markdown" } ));
 
-// Add the usual middleware, now with safe session support.
-/*bot.on("message", (ctx) => 
-  if (ctx.message.text === )
-    ctx.reply("Here's the unblocked link: 🔓 https://12ft.io/" + ctx.message.text));
-*/
+// Messages
 
 bot.on("msg", (ctx) => {
-  if (urlRegex.test(ctx.msg.text)) {
-      let modifiedUrls = ctx.msg.text.replace(urlRegex, "https://12ft.io/$1");
-      ctx.reply(`Here's the unblocked link: 🔓 ${modifiedUrls}`);
-      console.log('Link received:');
-      console.log(ctx.msg.text);
-      if(ctx.from.last_name === undefined) {
-        console.log('from', ctx.from.first_name, 'ID', ctx.from.id);}
-      else {
-        console.log('from', ctx.from.first_name, ctx.from.last_name, 'ID', ctx.from.id);}
-  }});
 
-// Run it concurrently.
+    // Logging
+
+    if (ctx.from.last_name === undefined) {
+      console.log('From:', ctx.from.first_name, '(@' + ctx.from.username + ')', 'ID:', ctx.from.id); }
+    else { console.log('From:', ctx.from.first_name, ctx.from.last_name, '(@' + ctx.from.username + ')', 'ID:', ctx.from.id); }
+    console.log("Message:", ctx.msg.text);
+
+    // Logic
+
+    if (!urlRegex.test(ctx.msg.text)) {
+      ctx.reply("*Send a link with a valid URL.*", { reply_to_message_id:ctx.message.message_id, parse_mode: "Markdown" }); }
+    else {
+      let modifiedUrls = ctx.msg.text.replace(urlRegex, "https://12ft.io/$1");
+      ctx.reply(`Here's the unblocked link: 🔓 ${modifiedUrls}`); }
+      
+  });
+
+// Error Handling
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error("Error while handling update", ctx.update.update_id, "\nQuery:", ctx.msg.text, "not found");
+  ctx.reply("Query: " + ctx.msg.text + " " + "not found!");
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+
+// Run
+
 console.log('Bot running. Please keep this window open or use a startup manager like PM2 to setup persistent execution and store logs.');
-// console.log('CTRL+C to terminate.');
-run(bot);
+bot.start();
